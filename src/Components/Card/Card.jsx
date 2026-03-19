dfdb332c99cfd5fcd8397f3b5baaeda68349a177
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-03-20 00:17:37 +0600

    Has find many of problem and fixed them

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,7 @@ import Limitations from '../Limitations/Limitations';
 const Card = ({ pricing }) => {
     const { features, limitations } = pricing;
     return (
-        <div className='bg-gradient-to-br from-gray-800/50 via-gray-900/80 to-black/50 p-8 sm:p-10 text-start rounded-2xl space-y-5 duration-1000 hover:-translate-y-3 min-h-[400px]'>
+        <div className='bg-gradient-to-br from-gray-800/50 via-gray-900/80 to-black/50 p-8 sm:p-10 text-start rounded-2xl space-y-5 duration-1000 hover:-translate-y-3'>
             <div className='flex items-center justify-between'>
                 <h2><span className='text-2xl'>{pricing.name}</span></h2>
 
@@ -13,7 +13,7 @@ const Card = ({ pricing }) => {
             </div>
 
             {
-                pricing.isPopular && <div class="badge badge-error bg-red-700 text-white rounded-full text-center w-full sm:w-[42%] py-1">Most Popular</div>
+                pricing.isPopular && <div class="badge badge-error bg-[#26ff0097] text-white rounded-full text-center w-[60%] sm:w-[44%] py-1">Most Popular</div>
             }
             <div className='flex items-center justify-between'>
                 <p>{pricing.desc}</p>
@@ -39,7 +39,7 @@ const Card = ({ pricing }) => {
             </div>
 
             <div className='flex items-end'>
-                <button className='btn btn-primary duration-1000 hover:bg-blue-800'>{pricing.btn}</button>
+                <button className='btn btn-primary duration-1000 hover:bg-blue-800 hover:border-blue-800'>{pricing.btn}</button>
             </div>
         </div>
     );
